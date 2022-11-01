0eccc0fd22da461ac332e9ffbdd605a80184a1d5
Rachel <racheljhumes@gmail.com>
2022-10-31 22:21:29 -0600

    commit tree 6603c3d8741f2aea7d2b522629e61e50004ffc2f

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function () {
+  let n = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  return n.slice(0,2);
+}
+
+const returnLastTwoDrivers = function () {
+  let n = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+  return n.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (x) {
+  return function (y) {
+    return x * y;
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (n, nToReturn) {
+  return nToReturn(n);
+}
